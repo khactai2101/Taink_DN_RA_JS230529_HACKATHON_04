fdbc9401c3f04cfe96f9eeb6ca0410206e4fc4a6
Khắc Tài Nguyễn <you@example.com>
2023-08-17 17:27:22 +0700

    hackathon4

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -14,27 +14,22 @@ interface CartProps {
 
 const Cart: React.FC<CartProps> = ({ cartItems }) => {
   const [data, setData] = useState<Product[]>(cartItems);
-  const [itemQuantities, setItemQuantities] = useState<{
-    [id: number]: number;
-  }>({});
 
   const handleDecrease = (id: number) => {
-    const updatedQuantities = { ...itemQuantities };
-    updatedQuantities[id] = (updatedQuantities[id] || 0) - 1;
-    setItemQuantities(updatedQuantities);
+    const item = data.find((item) => item.id === id);
 
-    const updatedData = data.map((item) =>
-      item.id === id ? { ...item, quantity: item.quantity - 1 } : item
-    );
-
-    setData(updatedData);
+    if (item && item.quantity > 0) {
+      const updatedData = data.map((item) =>
+        item.id === id ? { ...item, quantity: item.quantity - 1 } : item
+      );
+      setData(updatedData);
+    }
   };
 
   const handleIncrease = (id: number) => {
     const updatedData = data.map((item) =>
       item.id === id ? { ...item, quantity: item.quantity + 1 } : item
     );
-
     setData(updatedData);
   };
 
